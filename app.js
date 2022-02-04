8adf0cb71cda78f01155463cfa825be905b3b45e
Adnan Jusic <adnanjusic98@gmail.com>
2022-02-04 19:22:45 +0100

    Added UI and modified game logic

--- app.js
+++ app.js
@@ -1,65 +1,91 @@
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const newGame = document.querySelector(".newGame");
+const body = document.querySelector("body");
+const result = document.querySelector(".result");
+const commentary = document.querySelector(".commentary");
 let playerScore = 0;
 let computerScore = 0;
 let roundCount = 0;
 let playerSelection;
 let computerSelection;
-let body = document.querySelector("body");
-
-body.querySelector("button").addEventListener("click", function(){
-    game();
-});
-
-document.querySelector("button").addEventListener("onclick", function(){
-    console.log('made it');
-});
+let newDiv = document.createElement('p');
 
 function computerPlay() {
   let options = ["rock", "paper", "scissors"];
   return options[Math.floor(Math.random() * options.length)];
 }
 
-function playRound() {
+function playRound(e) {
   computerSelection = computerPlay();
-  playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+  playerSelection = e.target.className;
 
-  if (
-    (playerSelection === "rock" && computerSelection === "rock") ||
-    (playerSelection === "paper" && computerSelection === "paper") ||
-    (playerSelection === "scissors" && computerSelection === "scissors")
-  ) {
-    //return ;
-    console.log(`${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} can't beat ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`)
-    console.log(`Player ${playerScore} : ${computerScore} Computer`)
-  } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    
-    //return ;
-    console.log(`${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`)
-    console.log(`Player ${playerScore + 1} : ${computerScore} Computer`)
-    return playerScore += 1;
-  } else {
-    
-    //return ;
-    console.log(`${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} got beaten by ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`)
-    console.log(`Player ${playerScore} : ${computerScore + 1} Computer`)
-    return computerScore += 1;
+  if(playerScore === 5 || computerScore === 5){
+    commentary.appendChild(newDiv);
+    newDiv.innerHTML = 'Start a new game';
+  } else{
+    if (
+      (playerSelection === "rock" && computerSelection === "rock") ||
+      (playerSelection === "paper" && computerSelection === "paper") ||
+      (playerSelection === "scissors" && computerSelection === "scissors")
+    ) {
+      //return ;
+      commentary.innerHTML = `${
+        playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+      } can't beat ${
+        computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+      }`;
+      result.innerHTML = `Player ${playerScore} : ${computerScore} Computer`;
+    } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+      //return ;
+      commentary.innerHTML = `${
+        playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+      } beats ${
+        computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+      }`;
+      result.innerHTML = `Player ${playerScore + 1} : ${computerScore} Computer`;
+      playerScore += 1;
+      if (playerScore === 5) {
+        commentary.innerHTML = `<p style='font-weight: bold;'>You win!!</p>`;
+        return playerScore;
+      } else {
+        return playerScore;
+      }
+    } else {
+      //return ;
+      commentary.innerHTML = `${
+        playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+      } got beaten by ${
+        computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+      }`;
+      result.innerHTML = `Player ${playerScore} : ${computerScore + 1} Computer`;
+      computerScore += 1;
+      if (computerScore === 5) {
+        commentary.innerHTML = `<p style='font-weight: bold;'>Computer wins!!</p>`;
+        return computerScore;
+      } else {
+        return computerScore;
+      }
+    }
   }
+
+  
 }
 
-function game() {
-  if (playerScore == 5 || computerScore == 5) {
-    return `Game over in ${roundCount} rounds`
-  } else {
-    playRound();
-    roundCount++;
-    console.log(`Round ${roundCount}`)
-    game();
-  }
-}
+newGame.addEventListener("click", () => {
+  playerScore = 0;
+  computerScore = 0;
+  result.innerHTML = `Player ${playerScore} : ${computerScore} Computer`;
+  commentary.innerHTML = "";
+});
 
-
+rock.addEventListener("click", playRound);
+paper.addEventListener("click", playRound);
+scissors.addEventListener("click", playRound);
 
 /* Ask for an input from the user; Based on his input, the game should compare that input to the one of the computer and then decide which message it should display and which score should it update */
